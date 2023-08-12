240876b11e575658479c171b348ed1e8d6b13696
mrrobot911 <kelwin2705@gmail.com>
2023-08-12 21:26:26 +0300

    fix:  selector body

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const btn = document.querySelector("#burgerBtn");
 const test = document.querySelector(".menu");
+const body = document.querySelector(".body__container");
 function func(e){
     (btn.checked === true && e.target !== btn && e.target !== test && e.target !== test.childNodes[1]) && (btn.checked = false);
 }
-document.addEventListener('click', func);
+body.addEventListener('click', func);
 
